97d5ec3121368e31c436d580b934c1f3d951cd23
bybogdan <bgdnvarlamov@gmail.com>
2023-07-19 11:43:23 +0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = {
+}
+
+module.exports = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
